e4b4d1c86bd1a9ce8dc59471c2ff378c457b22ea
jakob_simmons <JSimmons01778@gmail.com>
2020-02-26 11:14:30 -0500

    Canvas Class

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,14 @@ import java.awt.*;
 public class Main {
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Tic Tac Toe - Hopps");
+        JFrame frame = new JFrame("Paint");
         frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
         int width = 720;
         int height = 719;
         frame.setPreferredSize(new Dimension(width, height + 24));
 
 
-        JPanel panel = new (width, height); //put whatever type of panel we want
+        JPanel panel = new (width, height); //put whatever type of panel we end up using
         panel.setFocusable(true);
         panel.grabFocus();
 
