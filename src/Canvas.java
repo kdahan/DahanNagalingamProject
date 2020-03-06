8a9828c9f0c418d22ad6c4e5ce0831fb623c3567
jakob_simmons <JSimmons01778@gmail.com>
2020-03-06 11:57:53 -0500

    Push for Dravin

--- src/Canvas.java
+++ src/Canvas.java
@@ -2,6 +2,15 @@ import javax.swing.*;
 
 public class Canvas extends JPanel{
 
+    private int width;
+    private int height;
+
+    public Canvas(int w, int h){
+        width = w;
+        height = h;
+        setSize(width, height);
+
+    }
 
 
 }
--- src/Main.java
+++ src/Main.java
@@ -20,4 +20,6 @@ public class Main {
         frame.setVisible(true);
         frame.setResizable(false);
     }
+
+
 }
